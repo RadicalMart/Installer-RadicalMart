acec5a2925e1fb66c04263a1dcd4809f8621b9cc
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-04-22 21:54:52 +0300

    поправил базу данных

--- sql/install/mysql/mysql.sql
+++ sql/install/mysql/mysql.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS `#__radicalinstaller_extensions`;
 CREATE TABLE `#__radicalinstaller_extensions`
 (
-    `id`           int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `title`        varchar(512) NOT NULL,
-    `type`         varchar(512) NOT NULL,
-    `element`      varchar(512) NOT NULL,
-    `folder`       varchar(512) NOT NULL,
-    `version`      varchar(100) NOT NULL,
-    `project_id`   int(10) unsigned NOT NULL,
-    `extension_id` int(10) unsigned NOT NULL,
+    `id`                    int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `title`                 varchar(512) NOT NULL,
+    `type_radicalinstaller` varchar(512) NOT NULL,
+    `type`                  varchar(512) NOT NULL,
+    `element`               varchar(512) NOT NULL,
+    `folder`                varchar(512) NOT NULL,
+    `version`               varchar(100) NOT NULL,
+    `project_id`            int(10) unsigned NOT NULL,
+    `extension_id`          int(10) unsigned NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
